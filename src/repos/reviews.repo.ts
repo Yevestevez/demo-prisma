3fffc84f9cd65b1a5da699e0055c6d7cce550929
Yevestevez <yevestevez.cg@gmail.com>
2026-05-11 19:10:53 +0200

    Add review methods to ReviewsRepo: getAllUserReviews, createReview, updateReview, and deleteReview

--- src/repos/reviews.repo.ts
+++ src/repos/reviews.repo.ts
@@ -2,6 +2,15 @@ import debug from 'debug';
 
 import { env } from '../config/env.ts';
 import type { AppPrismaClient } from '../config/db-config.ts';
+import type {
+    FilmReview,
+    ReviewBase,
+    UserReview,
+} from '../schemas/reviews/review.schema.ts';
+import type {
+    ReviewCreateDTO,
+    ReviewUpdateDTO,
+} from '../schemas/reviews/review.dto.ts';
 
 const log = debug(`${env.PROJECT_NAME}:repo:reviews`);
 log('Loading reviews repo...');
@@ -14,7 +23,7 @@ export class ReviewsRepo {
     }
 
     getAllFilmsReviews = async (filmID: number) => {
-        log(`Getting all reviews for film with id ${filmID}...`);
+        log(`Getting all reviews of film with id ${filmID}...`);
 
         return await this.#prisma.review.findMany({
             where: {
@@ -44,4 +53,124 @@ export class ReviewsRepo {
             },
         });
     };
+
+    async getAllUserReviews(userID: number): Promise<UserReview[]> {
+        log('Getting all reviews of user with id %s', userID);
+        return await this.#prisma.review.findMany({
+            where: {
+                userID,
+            },
+            omit: {
+                filmID: true,
+                userID: true,
+            },
+            include: {
+                user: {
+                    select: {
+                        profile: {
+                            select: {
+                                firstName: true,
+                                surname: true,
+                            },
+                        },
+                    },
+                },
+                film: {
+                    select: {
+                        title: true,
+                        year: true,
+                        director: true,
+                    },
+                },
+            },
+        });
+    }
+
+    async createReview(data: ReviewCreateDTO): Promise<FilmReview> {
+        log('Creating review for film %s by user %s', data.filmID, data.userID);
+        return await this.#prisma.review.create({
+            data: {
+                review: data.review,
+                rate: data.rate,
+                // date: new Date(),
+                filmID: data.filmID,
+                userID: data.userID,
+            },
+            omit: {
+                filmID: true,
+                userID: true,
+            },
+            include: {
+                user: {
+                    select: {
+                        profile: {
+                            select: {
+                                firstName: true,
+                                surname: true,
+                            },
+                        },
+                    },
+                },
+                film: {
+                    select: {
+                        title: true,
+                    },
+                },
+            },
+        });
+    }
+
+    async updateReview(
+        userID: number,
+        filmID: number,
+        data: ReviewUpdateDTO,
+    ): Promise<FilmReview> {
+        log('Updating review for film %s by user %s', filmID, userID);
+        return await this.#prisma.review.update({
+            where: {
+                userID_filmID: {
+                    userID,
+                    filmID,
+                },
+            },
+            data,
+            omit: {
+                filmID: true,
+                userID: true,
+            },
+            include: {
+                user: {
+                    select: {
+                        profile: {
+                            select: {
+                                firstName: true,
+                                surname: true,
+                            },
+                        },
+                    },
+                },
+                film: {
+                    select: {
+                        title: true,
+                    },
+                },
+            },
+        });
+    }
+
+    async deleteReview(userID: number, filmID: number): Promise<ReviewBase> {
+        log('Deleting review for film %s by user %s', filmID, userID);
+        return await this.#prisma.review.delete({
+            where: {
+                userID_filmID: {
+                    userID,
+                    filmID,
+                },
+            },
+            omit: {
+                filmID: true,
+                userID: true,
+            },
+        });
+    }
 }
