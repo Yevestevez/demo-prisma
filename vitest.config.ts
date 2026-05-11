e326dec96810d621263888487d3481d42f487b61
Eduardo Yeves <yevestevez.cg@gmail.com>
2026-05-11 09:39:45 +0100

    Add new configuration for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,17 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-    test: { globals: true, setupFiles: './src/config/setup-test.ts' },
+    test: {
+        globals: true,
+        setupFiles: './src/config/setup-test.ts',
+        include: ['**/*.test.ts', '**/*.test.tsx'],
+        coverage: {
+            include: ['src/**/*.ts'],
+            exclude: [
+                'src/index.ts',
+                'src/**/entities/*.ts',
+                'src/**/types/*.ts',
+            ],
+        },
+    },
 });
