9be4e87a923b14fada81cdd9af4ae1231e84e602
Yevestevez <yevestevez.cg@gmail.com>
2026-05-13 11:45:56 +0200

    Enhance UsersRepo tests with additional methods and error handling

--- src/repos/users.repo.test.ts
+++ src/repos/users.repo.test.ts
@@ -1,6 +1,11 @@
 import type { Mock } from 'vitest';
 import type { AppPrismaClient } from '../config/db-config.ts';
-import type { RegisterUserDTO } from '../schemas/users/user.dto.ts';
+import type {
+    LoginUserDTO,
+    ProfileUpdateDTO,
+    RegisterUserDTO,
+    UserUpdateDTO,
+} from '../schemas/users/user.dto.ts';
 import type { User } from '../schemas/users/user.schema.ts';
 import { AuthService } from '../services/auth.ts';
 import { UsersRepo } from './users.repo.ts';
@@ -17,7 +22,7 @@ describe('GIVEN an instance of <UsersRepo> class', () => {
                 findUniqueOrThrow: vi.fn().mockResolvedValue({}),
                 create: vi.fn().mockResolvedValue({}),
                 update: vi.fn().mockResolvedValue({}),
-                delete: vi.fn().mockRejectedValue({}),
+                delete: vi.fn().mockResolvedValue({}),
             },
         } as unknown as AppPrismaClient;
 
@@ -55,7 +60,7 @@ describe('GIVEN an instance of <UsersRepo> class', () => {
                     email: 'test@email.com',
                     password: 'hashed-password',
                     role: 'USER',
-                });
+                } as LoginUserDTO);
 
                 vi.spyOn(AuthService, 'compare').mockResolvedValueOnce(true);
 
@@ -91,6 +96,112 @@ describe('GIVEN an instance of <UsersRepo> class', () => {
                         password: '123456',
                     }),
                 ).rejects.toThrow('Invalid user or password');
+            });
+        });
+    });
+
+    describe('WHEN method <getAllUsers> is called', () => {
+        test('THEN it should return users array', async () => {
+            const users = await repo.getAllUsers();
+
+            expect(prismaMock.user.findMany).toHaveBeenCalled();
+            expect(users).toEqual([]);
+        });
+    });
+
+    describe('WHEN method <getUserById> is called', () => {
+        describe('And the user with given id exists', () => {
+            test('THEN it should return the user', async () => {
+                const user = await repo.getUserById(1);
+
+                expect(prismaMock.user.findUniqueOrThrow).toHaveBeenCalled();
+                expect(user).toEqual({});
+            });
+        });
+
+        describe('And the user with given id does NOT exists', () => {
+            test('THEN it should throw an error', async () => {
+                (
+                    prismaMock.user.findUniqueOrThrow as Mock
+                ).mockRejectedValueOnce(new Error('User not found'));
+
+                await expect(repo.getUserById(999)).rejects.toThrow(
+                    'User not found',
+                );
+            });
+        });
+    });
+
+    describe('WHEN method <updateUserById> is called', () => {
+        describe('And the user with given id exists', () => {
+            test('THEN it should return the updated user', async () => {
+                const updatedUser = await repo.updateUserById(1, {
+                    password: '123456',
+                } as UserUpdateDTO);
+
+                expect(prismaMock.user.update).toHaveBeenCalled();
+                expect(updatedUser).toEqual({} as User);
+            });
+        });
+
+        describe('And the user with given id does NOT exists', () => {
+            test('THEN it should throw an error', async () => {
+                (prismaMock.user.update as Mock).mockRejectedValueOnce(
+                    new Error('User not found'),
+                );
+
+                await expect(
+                    repo.updateUserById(999, { password: '123456' }),
+                ).rejects.toThrow('User not found');
+            });
+        });
+    });
+
+    describe('WHEN method <updateUserProfile> is called', () => {
+        describe('And the user with given id exists', () => {
+            test('THEN it should return the updated user', async () => {
+                const updatedUser = await repo.updateUserProfile(
+                    1,
+                    {} as ProfileUpdateDTO,
+                );
+
+                expect(prismaMock.user.update).toHaveBeenCalled();
+                expect(updatedUser).toEqual({} as User);
+            });
+        });
+
+        describe('And the user with given id does NOT exists', () => {
+            test('THEN it should throw an error', async () => {
+                (prismaMock.user.update as Mock).mockRejectedValueOnce(
+                    new Error('User not found'),
+                );
+
+                await expect(
+                    repo.updateUserProfile(999, {} as ProfileUpdateDTO),
+                ).rejects.toThrow('User not found');
+            });
+        });
+    });
+
+    describe('WHEN method <deleteUserById> is called', () => {
+        describe('And the user with given id exists', () => {
+            test('THEN it should return the deleted user', async () => {
+                const deletedUser = await repo.deleteUserById(1);
+
+                expect(prismaMock.user.delete).toHaveBeenCalled();
+                expect(deletedUser).toEqual({} as User);
+            });
+        });
+
+        describe('And the user with given id does NOT exists', () => {
+            test('THEN it should throw an error', async () => {
+                (prismaMock.user.delete as Mock).mockRejectedValueOnce(
+                    new Error('User not found'),
+                );
+
+                await expect(repo.deleteUserById(999)).rejects.toThrow(
+                    'User not found',
+                );
             });
         });
     });
